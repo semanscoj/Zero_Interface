9a816f58fa860dacefd9724628b763bebc81d0e5
Jeremee Semansco <Jeremee.Semansco@gmail.com>
2016-08-30 18:41:24 -0400

    Added Matrix tab stats

---
+++ Matrix/Deck.py
@@ -0,0 +1,75 @@
+import json, os
+from Utils.Attributes import AttributeInterface
+
+
+class DeckManager:
+    def __init__(self, state):
+        self.state = state
+        self._decks = None
+        self.programs = []
+        self.setup()
+
+    def setup(self):
+        print('looking in', os.getcwd(), 'for decks.json')
+
+    def deck_names(self, print_now=False, pre='', post=''):
+        for i in self.state.decks:
+            name = i.get("DECK")
+            if print_now:
+                print(pre, name, post)
+        return [pre + i.get("DECK") + post for i in self.state.decks]
+
+    def get_by_name(self, name):
+        for i in self.state.decks:
+            if str(i) == name:
+                return i
+        return self.state.decks[0]
+
+
+class Deck(AttributeInterface):
+    def __init__(self, state, dict):
+        self.state = state
+        self.stats = ['Sleaze', 'Attack', 'Firewall', 'Data Processing']
+        AttributeInterface.__init__(self, 'attributes', self.stats)
+        self.dict = dict
+        self.array = []
+        self.assigned_array = []
+        self.setup()
+
+    def setup(self):
+        for key, value in self.dict.items():
+            try:
+                setattr(self, key, value)
+            except:
+                print('Dict pair not set in deck object')
+                pass
+
+        if 'ARRAY' in self.dict:
+            try:
+                self.array = [int(i.strip()) for i in self.dict['ARRAY'].split(' ')]
+            except:
+                print('Got non number in array')
+                self.array = range(1, 5)
+
+    def get(self, key):
+        if key in self.dict.keys():
+            return getattr(self, key)
+        else:
+            return None
+
+    def show_info(self):
+
+        str = ''
+        for k,v in self.dict.items():
+            str += '%s: %s\n' % (k,v)
+        print(str)
+        return str
+
+    def __str__(self):
+        return str(self.get('DECK'))
+
+if __name__ == '__main__':
+    manager = DeckManager()
+    deck = manager.decks[0]
+
+    print(deck)
---
+++ Matrix/Program.py
@@ -0,0 +1,36 @@
+class Program:
+    def __init__(self, detail_dict):
+        self.detail_dict = detail_dict
+        self.name = None
+        self.effect = None
+        self.avail = None
+        self.cost = None
+        self.source = None
+        self.attribute_mod = {}
+        self.running_in = None
+        self.setup()
+
+    def setup(self):
+        try:
+            self.name = self.detail_dict['name']
+            self.effect = self.detail_dict['effect']
+            self.avail = self.detail_dict['avail']
+            self.cost = self.detail_dict['cost']
+            self.source = self.detail_dict['source']
+        except Exception as e:
+            print(e)
+        print('Loaded program', self.name)
+
+    def load(self, deck_obj):
+        if self.running_in:
+            self.unload()
+
+        for k, v in self.attribute_mod.items():
+            deck_obj.set(k, self.name, v)
+        self.running_in = deck_obj
+
+    def unload(self):
+        if self.running_in:
+            for k, v in self.attribute_mod.items():
+                self.running_in.del_mod(k, self.name)
+            self.running_in = None
